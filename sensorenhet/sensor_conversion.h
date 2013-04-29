f81e7985ceafe2b8e0300b28bb66baca4487afe6
Martin <maran703@student.liu.se>
2013-04-29 11:59:03 +0200

    makeTurn svnger men inte riktigt 90 grader

--- sensorenhet/sensor_conversion.h
+++ sensorenhet/sensor_conversion.h
@@ -33,10 +33,10 @@
 #define LINE_FOLLOWING 0x05
 
 #define NR_OF_GYRO_SAMPLES 10
-#define GYRO_REF_LEVEL 127
+#define GYRO_REF_LEVEL 128
 /* Kompensering för att gyrot driver:
   hundradelsgrader/sample * 10340 = GYRO_COMP  */
-#define GYRO_COMP -25000
+#define GYRO_COMP 0 //-20000
 
 #define CONVERSION_ERROR 1;
 
