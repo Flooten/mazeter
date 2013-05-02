060c8cc828edcf8df110c646a94480500b4df3d9
Herman ISY <herek331@student.liu.se>
2013-05-02 15:00:28 +0200

    lookup.h fixed

--- sensorenhet/look_up.h
+++ sensorenhet/look_up.h
@@ -14,13 +14,13 @@
 #include <stdint.h>
 #include <avr/pgmspace.h>
 
-extern const uint8_t distance1_table[34][2];
-extern const uint8_t distance2_table[34][2];
-extern const uint8_t distance3_table[34][2];
-extern const uint8_t distance4_table[34][2];
+extern const uint8_t distance1_table[37][2];
+extern const uint8_t distance2_table[37][2];
+extern const uint8_t distance3_table[37][2];
+extern const uint8_t distance4_table[37][2];
 extern const uint8_t distance5_table[27][2];
 extern const uint8_t distance6_table[27][2];
-extern const uint8_t distance7_table[34][2];
+extern const uint8_t distance7_table[37][2];
 
 uint8_t lookUp(uint8_t raw_value, uint8_t size, const uint8_t table[][2]);
 uint8_t lookUpDistance(uint8_t raw_value, uint8_t sensor);
