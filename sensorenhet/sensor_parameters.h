5b20e68e84af0c27a557279d41c76e8b5fe17847
Martin <maran703@student.liu.se>
2013-05-16 11:36:57 +0200

    comment sensor, missed one file before

--- sensorenhet/sensor_parameters.h
+++ sensorenhet/sensor_parameters.h
@@ -1,12 +1,12 @@
 ﻿/*
- * FILNAMN:       raw_data.h
+ * FILNAMN:       sensor_parameters.h
  * PROJEKT:       Mazeter
  * PROGRAMMERARE: Herman Ekwall
  *                Mattias Fransson
  *                
- * DATUM:         2013-04-15
+ * DATUM:         2013-05-17
  *
- * BESKRIVNING:
+ * BESKRIVNING: Parameterar för linjehantering
  *
  */
 
